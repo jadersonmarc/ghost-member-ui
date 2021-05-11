c6f60119228332366c6e2ba92a5f31d207b9efa5
jadersonmarc <jadersonmarc@gmail.com>
2021-05-10 22:25:54 -0300

    add card box

--- src/components/organisms/HeaderHome.vue
+++ src/components/organisms/HeaderHome.vue
@@ -3,7 +3,7 @@
 <template>
 	<div class="text-center ">
 		<div class="bg-cover">
-			<h1 class="text-header font-italic font-weight-medium">ghost member</h1>
+			<h1 class="text-header font-italic font-weight-medium">Ghost Member</h1>
 			<nav class="nav-fill">
 				<ul class="nav-list">
 					<li class="nav-item">
@@ -25,6 +25,59 @@
 					</li>
 				</ul>
 			</nav>
+			<div class="ml-0 mr-0 pb-1 nav-news">
+				<nav class="navbar navbar-expand-md">
+					<div id="my-nav" class="">
+						<ul class="navbar-nav mx-auto">
+							<li class="nav-item">
+								<a class="nav-link" href="article.html">NEWS</a>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link" href="article.html">HISTORY</a>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link" href="#">CULTURE</a>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link" href="#">TECH</a>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link" href="#">LIFE</a>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link" href="#">OPINION</a>
+							</li>
+							<li class="nav-item">
+								<form action="" method="POST">
+									<div class="input-group mt-0 mx-auto" style="width:16px;">
+										<div class=""></div>
+									</div>
+								</form>
+							</li>
+						</ul>
+					</div>
+				</nav>
+			</div>
+		</div>
+		<div class="container__img_header">
+			<v-card class="my-card">
+				<img src="images/left-img.jpg" />
+				<div class="header__seemore">
+					<p>Spirituality</p>
+					<hr />
+					<h2 class="font-weight-black">Free template designed by FreeHTML5.co</h2>
+					<a href="article.html" class="btn">READ MORE</a>
+				</div>
+			</v-card>
+			<v-card class="my-card">
+				<img src="images/right-img.jpg" />
+				<div class="header__seemore">
+					<p>Spirituality</p>
+					<hr />
+					<h2 class="">Free template designed by FreeHTML5.co</h2>
+					<a href="article.html" type="button" class="btn">READ MORE</a>
+				</div>
+			</v-card>
 		</div>
 	</div>
 </template>
