f9d570bafa9a25b7f948b77ac043e1eb70a9d74d
marc jaderson <jadersonmarc@gmail.com>
2021-07-22 15:18:04 -0300

    alter vue config webpack

--- vue.config.js
+++ vue.config.js
@@ -5,7 +5,9 @@ module.exports = {
         const types = ['vue-modules', 'vue', 'normal-modules', 'normal']
         types.forEach(type => addStyleResource(config.module.rule('stylus').oneOf(type)))
     },
-
+    devServer: {
+        disableHostCheck: true
+    }
 
 }
 
