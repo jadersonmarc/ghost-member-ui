26024474cac2987f9163b48aa773bfa23292c82c
marc jaderson <jadersonmarc@gmail.com>
2021-07-25 00:36:46 -0300

    fix position img

--- src/components/molecules/HeaderHighlight.vue
+++ src/components/molecules/HeaderHighlight.vue
@@ -1,9 +1,9 @@
 <!-- @format -->
 
 <template>
-	<v-row class="justify-space-around flex-wrap mx-2 ">
-		<v-img src="images/architecture.png" max-height="180" max-width="310"
-			><h5 class="pa-12 title highlight__item font-weight-black">Architecture</h5></v-img
+	<v-row class="justify-space-between flex-wrap mx-2 ">
+		<v-img src="images/architecture.png" max-height="180" max-width="310" class="ml-1"
+			><h5 class="pa-12  title highlight__item font-weight-black">Architecture</h5></v-img
 		>
 		<v-img src="images/interior.png" max-height="180" max-width="310"
 			><h5 class="pa-12 title highlight__item font-weight-black">Interior</h5></v-img
@@ -11,8 +11,8 @@
 		<v-img src="images/food.png" max-height="180" max-width="310"
 			><h5 class="pa-12 title highlight__item font-weight-black">Food</h5></v-img
 		>
-		<v-img src="images/travel.png" max-height="180" max-width="310"
-			><h5 class="pa-12 title highlight__item font-weight-black">Travel</h5></v-img
+		<v-img src="images/travel.png" max-height="180" max-width="310" class="mr-1"
+			><h5 class="pa-12  title highlight__item font-weight-black">Travel</h5></v-img
 		>
 	</v-row>
 </template>
