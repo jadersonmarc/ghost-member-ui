6eeeace7271a8cc3de084bb9369d2b0e13706b36
marc jaderson <jadersonmarc@gmail.com>
2021-07-24 22:36:38 -0300

    add body

--- src/components/pages/Home.vue
+++ src/components/pages/Home.vue
@@ -4,7 +4,7 @@
   <div class="home__container">
     <HeaderHome class="home__header"/>
     <div class="home__content">
-      <div class="boby_header__lg ">
+      <div class="body_header__lg ">
         <v-row class="d-flex">
           <v-col>
             <v-img height="100%" width="100%" src="images/gallery-img1.png" rounded="0"></v-img>
@@ -22,7 +22,7 @@
         </v-row>
       </div>
 
-      <div class="boby_header__sm">
+      <div class="body_header__sm">
         <v-img height="100%" width="100%" src="images/video-cover1.jpg" class="d-flex align-center" rounded="0">
           <div class="opacity text-center d-flex flex-column justify-content-center">
             <a class="align-self-center pb-2" href="https://www.youtube.com/watch?v=vpO8sZDxOGI"><img
@@ -32,53 +32,47 @@
         </v-img>
       </div>
 
-      <div class="boby_content__lg">
-        <div class="card">
-          <img class="card-img h-100" src="images/gallery-img2.jpg" alt="">
-          <div class="card-img-overlay">
-            <div class="contact-box">
-              <p><small>march 27, 2020</small></p>
-              <h3>Trendy Summer
-                Fashion</h3>
-              <p>To take a trivial example, which of us ever undertakes laborious physical exercise,
-                some advantage from it? fault with a man who chooses...</p>
-            </div>
-          </div>
-        </div>
+      <div class="body_content__lg">
+        <v-img height="100%" width="100%" src="images/gallery-img2.jpg" rounded="0"></v-img>
       </div>
 
       <div class="body_content__sm">
-        <div class="card">
-          <img class="card-img-top" src="images/gallery-img3.jpg" alt="">
-          <div class="card-body bg-gray cus-pd2 cus-arrow-up">
+        <v-row class="d-flex flex-column">
+          <v-col>
+            <v-img height="100%" width="100%" src="images/gallery-img3.jpg" rounded="0"></v-img>
+          </v-col>
+          <v-col class="text_body__header">
             <p><small>march 27, 2020</small></p>
             <h3>Trendy Summer
               Fashion</h3>
-            <p>To take a trivial example, which of us ever undertakes laborious physical exercise, some
-              advantage from it? fault with a man who chooses...</p>
-          </div>
-        </div>
-      </div>
-
-      <div class="boby_footer__lg">
-        <v-row class="d-flex">
-        <v-col>
-          <v-img height="100%" width="100%" src="images/gallery-img4.jpg" rounded="0"></v-img>
-        </v-col>
-        <v-col class="text_body__header">
-          <p><small>march 27, 2020</small></p>
-          <h3>Trendy Summer
-            Fashion</h3>
-          <p>
-            To take a trivial example, which of us ever undertakes laborious physical exercise,
-            except to obtain some advantage from it? But who has any right to find fault with a man
-            who chooses...
-          </p>
-        </v-col>
+            <p>
+              To take a trivial example, which of us ever undertakes laborious physical exercise,
+              except to obtain some advantage from it? But who has any right to find fault with a man
+              who chooses...
+            </p>
+          </v-col>
         </v-row>
       </div>
 
-      <div class="boby_footer__sm">
+      <div class="body_footer__lg">
+        <v-row class="d-flex">
+          <v-col>
+            <v-img height="100%" width="100%" src="images/gallery-img4.jpg" rounded="0"></v-img>
+          </v-col>
+          <v-col class="text_body__header">
+            <p><small>march 27, 2020</small></p>
+            <h3>Trendy Summer
+              Fashion</h3>
+            <p>
+              To take a trivial example, which of us ever undertakes laborious physical exercise,
+              except to obtain some advantage from it? But who has any right to find fault with a man
+              who chooses...
+            </p>
+          </v-col>
+        </v-row>
+      </div>
+
+      <div class="body_footer__sm">
         <v-img height="100%" width="100%" src="images/video-cover2.jpg" class="d-flex align-center" rounded="0">
           <div class="opacity text-center d-flex flex-column justify-content-center">
             <a class="align-self-center pb-2" href="https://www.youtube.com/watch?v=vpO8sZDxOGI"><img
